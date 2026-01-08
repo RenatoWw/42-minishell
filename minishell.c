fd9b6eaca24dfc6b2282856a34aa6badaace3e5c
Renato <renatoflud2017@hotmail.com>
2026-01-08 19:14:44 -0300

    initial commit

---
+++ minishell.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranhaia- <ranhaia-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 16:10:49 by ranhaia-          #+#    #+#             */
+/*   Updated: 2026/01/08 19:12:39 by ranhaia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(void)
+{
+	char	*line;
+
+	using_history();
+	while (1)
+	{
+		line = readline("> ");
+		if (!line)
+			break ;
+		if (line != "")
+			add_history(line);
+		printf("%s\n", line);
+		free(line);
+	}
+	clear_history();
+	return (0);
+}
---
+++ minishell.h
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ranhaia- <ranhaia-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 18:22:27 by ranhaia-          #+#    #+#             */
+/*   Updated: 2026/01/08 18:45:14 by ranhaia-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+typedef enum e_token_type
+{
+	TOKEN_WORD,
+	TOKEN_PIPE,
+	TOKEN_REDIRECT_IN,
+	TOKEN_REDIRECT_OUT,
+	TOKEN_APPEND,
+	TOKEN_HEREDOC,
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+	struct s_token	*prev;
+}	t_token;
+
+typedef struct s_cmd
+{
+	char			**cmd_args;
+	char			*cmd_path;
+	int				fd_in;
+	int				fd_out;
+	pid_t			process_pid;
+	struct s_cmd	*next;
+	struct s_cmd	*prev;
+}	t_cmd;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+	struct s_env	*prev;
+}	t_env;
+
+typedef struct s_mini
+{
+	t_env	*env_list;
+	t_token	*tokens;
+	t_cmd	*cmd;
+	char	*input;
+	int		exit_code;
+	int		original_stdin;
+	int		original_stdout;
+}	t_mini;
+
+#endif
